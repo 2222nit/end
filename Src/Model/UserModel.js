5e33d57e6d5097e7ab3d88ed05b33910cd4757fd
2222nit <nitishkamboj8070@gmail.com>
2026-04-03 12:49:47 +0530

    Add files via upload

---
+++ Src/Model/UserModel.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+
+
+const userscheme = new mongoose.Schema({
+    name:{ Type:String, required:true,trim:true},
+    email:{ Type:String,required:true,trim:true},
+    password:{ Type:String,required:true,trim:true},
+
+},
+{timeseriestamps:true}
+
+)
+
+module.exports = mongoose.model("user",userscheme)
